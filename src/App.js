bc914e9328f480360eb51f980d8957baa532fcaa
Ivan Shitikov <ifinesse2406@gmail.com>
2021-03-16 15:15:15 +0200

    second coomit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,43 @@
 import './App.css';
-import React, {Component} from 'react';
-import {CardList} from "./components/card-list/card-list.component";
-import {SearchBox} from "./components/search-box/search-box.component"
+import React, { Component } from 'react';
+import { CardList } from "./components/card-list/card-list.component";
+import { SearchBox } from "./components/search-box/search-box.component"
 
 class App extends Component {
-  constructor () {
+  constructor() {
     super();
     this.state = {
       monsters: [],
       searchField: "",
     }
   }
-  componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
-    .then(response => response.json())
-    .then(users => this.setState({monsters: users}))
+  async componentDidMount() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const users = await response.json();
+    this.setState({monsters: users});
+      // .then(response => response.json())
+      // .then(users => this.setState({ monsters: users }))
   }
 
-  render( ) {
-    const {monsters, searchField} = this.state;
+  render() {
+    const { monsters, searchField } = this.state;
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()));
-    
+
     {
-      
+
     }
-    
+
     return (
       <div className="App">
-        {/* <input type="search" placeholder="search" onChange={e => {this.setState(
-          {searchField: e.target.value}, () => {console.log(filteredMonsters)}
-          );
-          }}/> */}
-          <SearchBox placeholder="search" handleChange={e => {
-                this.setState({ searchField: e.target.value });
-            }}/>
-        <CardList monsters={filteredMonsters} />      
+        <SearchBox placeholder="search" handleChange={e => {
+          this.setState({ searchField: e.target.value });
+        }} />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
 
-  
+
 }
 
 export default App;
